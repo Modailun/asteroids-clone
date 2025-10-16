ad5a62b58b43fbda00ac52a44416210a5207352f
Modailun <modailun@gmail.com>
2025-10-16 21:13:40 +0200

    Added asteroids movements

--- scripts/asteroids.gd
+++ scripts/asteroids.gd
@@ -1,4 +1,17 @@
 extends Node2D
 
+@onready var screen_size = get_viewport_rect().size
+
+func _ready() -> void:
+	var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+	rotation = direction.angle()
+
+func _physics_process(delta: float) -> void:
+	position.x = wrapf(position.x, 0, screen_size.x)
+	position.y = wrapf(position.y, 0, screen_size.y)
+
+	position += transform.x * 32 * delta
+
+
 func died() -> void:
 	print("Asteroid died")
