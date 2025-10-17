6dace44c848709331fbafaebbe42242f1763ae12
Modailun <modailun@gmail.com>
2025-10-17 22:55:50 +0200

    Created Asteroids class and resources

---
+++ resources/asteroid_resource.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name AsteroidResource
+
+@export var speed: float
+@export var type: types
+
+enum types {BIG, MEDIUM, SMALL}
--- scripts/asteroids.gd
+++ scripts/asteroids.gd
@@ -1,6 +1,8 @@
-extends Node2D
+class_name Asteroids extends Node2D
 
 @onready var screen_size = get_viewport_rect().size
+
+@export var asteroid_stats = Resource
 
 func _ready() -> void:
 	var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
@@ -10,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	position.x = wrapf(position.x, 0, screen_size.x)
 	position.y = wrapf(position.y, 0, screen_size.y)
 
-	position += transform.x * 32 * delta
+	position += transform.x * asteroid_stats.speed * delta
 
 
 func died() -> void:
